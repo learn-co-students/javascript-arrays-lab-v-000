62f179a8752ada6ce524c6003be92de0489a3a4b
Daniel Suarez <suarez.daniel101@gmail.com>
2017-01-10 05:50:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const app = "I don't do much."
+
+var kittens = ['milo', 'otis', 'garfield'];
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(name){
+  kittens.pop(name);
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift(name);
+  return kittens;
+}
+
+function appendKitten(name){
+  var array = [...kittens, name];
+  return array;
+}
+
+function prependKitten(name){
+  var array = [name, ...kittens];
+  return array;
+}
+
+function removeLastKitten(){
+  var array = kittens.splice(0, 1);
+  return array;
+}
