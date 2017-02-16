075b1035b5e2f07eea1fa699b9af81eb8a586c0f
Brad Watson <brad.watson.orlando@gmail.com>
2017-02-16 14:45:47 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  return [...kittens, name]
+}
+
+function prependKitten(name) {
+  return [name, ...kittens]
+}
+
+function removeLastKitten() {
+  var kitties = kittens.slice(0, kittens.length - 1)
+  return kitties
+}
+
+function removeFirstKitten() {
+  var kitties = kittens.slice(1)
+  return kitties
+}
