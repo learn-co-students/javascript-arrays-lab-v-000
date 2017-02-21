c9f9bcbe890f20884a8bd4f0165971dfb9eee5a9
bmolina-nyc <bruce.molina.81@gmail.com>
2017-02-20 20:13:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const app = "I don't do much."
+const kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+  return kittens.push(name)
+}
+
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name)
+}
+
+
+function destructivelyRemoveLastKitten(){
+  return kittens.pop(1)
+}
+
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift(1)
+}
+
+function appendKitten(name){
+  var arr = kittens 
+  kittens.push(name)
+} 
+
+  // 2) Arrays prependKitten(name) prepends a kitten to the kittens array and returns a new array, leaving the kittens array unchanged:
+  //    ReferenceError: prependKitten is not defined
+  //     at Context.<anonymous> (test/index-test.js:53:14)
+
+  // 3) Arrays removeLastKitten() removes the last kitten in the kittens array and returns a new array, leaving the kittens array unchanged:
+  //    ReferenceError: removeLastKitten is not defined
+  //     at Context.<anonymous> (test/index-test.js:61:14)
+
+  // 4) Arrays removeFirstKitten() removes the first kitten from the kittens array and returns a new array, leaving the kittens array unchanged:
+  //    ReferenceError: removeFirstKitten is not defined
+  //     at Context.<anonymous> (test/index-test.js:69:14)
+
