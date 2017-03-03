988a5f28bab6f0adbd8e47fef54f98ce00ec0983
davdkm <davd.km@gmail.com>
2017-03-02 17:00:16 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+var destructivelyAppendKitten = kitten => {
+  kittens.push(kitten);
+  return kittens;
+}
+
+var destructivelyPrependKitten = kitten => {
+  kittens.unshift(kitten);
+  return kittens;
+}
+
+var destructivelyRemoveLastKitten = () => {
+  kittens.pop();
+  return kittens;
+}
+
+var destructivelyRemoveFirstKitten = () => {
+  kittens.shift();
+  return kittens;
+}
+
+var appendKitten = (kitten) => {
+  return [...kittens, kitten];
+}
+
+var prependKitten = (kitten) => {
+  return [kitten, ...kittens];
+}
+
+var removeLastKitten = () => {
+  return kittens.slice(0, kittens.length - 1);
+}
+
+var removeFirstKitten = () => {
+  return kittens.slice(1);
+}
