74c669baf420091d67c9701df1f83f7e9fd76a18
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-04-06 15:48:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name) {
+  var kek = [...kittens, name]
+  return kek
+}
+
+function prependKitten(name) {
+  var kek = [name, ...kittens]
+  return kek
+}
+
+function removeLastKitten() {
+  var kek = kittens.slice(0, kittens.length-1)
+  return kek
+}
+
+function removeFirstKitten() {
+  var kek = kittens.slice(1)
+  return kek
+}
