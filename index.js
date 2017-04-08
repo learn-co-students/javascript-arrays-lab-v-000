36881bdf3b35befb6dffd72b86ad76a8efbede26
joel <joelbitar@protonmail.com>
2017-04-08 16:18:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-const app = "I don't do much."
+function kittens() {
+  var kittens = ["Milo", "Otis", "Garfield"];
+}
+
+function destructivelyAppendKitten(name) {
+  kittens.push('Ralph');
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift('Bob');
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  var x = kittens.slice();
+  x.push(name);
+  return x;
+}
+
+function prependKitten(name) {
+  var x = kittens.slice();
+  x.unshift(name);
+  return x;
+}
+
+function removeLastKitten() {
+  var x = kittens.slice();
+  x.pop();
+  return x;
+}
+
+function removeFirstKitten() {
+  var x = kittens.slice();
+  x.shift();
+  return x;
+}
