54763fc5ee00bd9b3cf9fbfa42d86a30a52a9d94
Andrew Bunys <awbunys@gmail.com>
2017-04-12 03:20:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  var removed = kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  var removed = kittens.shift();
+  return removed;
+}
+
+function appendKitten(name) {
+  var newKittens = kittens.slice(0);
+  newKittens.push(name);
+  return newKittens;
+}
+
+function prependKitten(name) {
+  var newKittens = kittens.slice(0);
+  newKittens.unshift(name);
+  return newKittens;
+}
+
+function removeLastKitten() {
+  var newKittens = kittens.slice(0);
+  var removed = newKittens.pop();
+  return newKittens;
+}
+
+function removeFirstKitten() {
+  var newKittens = kittens.slice(0);
+  var removed = newKittens.shift();
+  return newKittens;
+}
