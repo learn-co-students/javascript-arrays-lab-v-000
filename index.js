f78468fab3387d75bf5f3576f54042eb9471e7ff
umohm1 <umohm12@gmail.com>
2017-04-15 16:27:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+
+function destructivelyAppendKitten(name) {
+   kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop(name)
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift(name)
+}
+
+function appendKitten(name) {
+  return [...kittens, name] //adds to back
+}
+
+function prependKitten(name) {
+  return [name, ...kittens] //adds to front
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
+
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length-1)
+}
