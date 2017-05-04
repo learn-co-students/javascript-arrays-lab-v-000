94bcf6ab78db3b272badb16de6b246ab0a2c8e0d
Malind Kowalski <malind.nikulski@gmail.com>
2017-05-04 12:55:13 -0600

    working on js array lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const app = "I don't do much."
+// const app = "I don't do much."
+
+kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  return kittens.push(name)
+}
+
+function destructivelyPrependKitten() {
+
+}
