7bbda5a7f87b0b1c0d264caa252ee21782d6667e
James Randolph <james.randolph2016@gmail.com>
2017-05-11 02:32:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name) {
+  var morekittens = [...kittens, "Broom"]
+  return morekittens
+}
+
+function prependKitten(name) {
+  var morekittens = ["Arnold", ...kittens]
+  return morekittens
+}
+
+function removeLastKitten() {
+  var morekittens = kittens.slice(0, kittens.length - 1)
+  return morekittens
+}
+
+function removeFirstKitten() {
+  var morekittens = kittens.slice(-2)
+  return morekittens
+}
