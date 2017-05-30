df2d9c0c4c0653cc6a0065e32a7c2e155b8e9ff8
brownielox <mrsmpython@gmail.com>
2017-05-30 17:10:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift()
+  }
+
+function appendKitten(name) {
+  var new_kittens = [...kittens, name]
+  return new_kittens
+}
+
+function removeLastKitten() {
+  return kittens.slice(-kittens.length)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
+
+function prependKitten(name) {
+  var new_kittens = [name, ...kittens]
+  return new_kittens
+}
