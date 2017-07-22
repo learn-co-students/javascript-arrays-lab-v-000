0810d1d1bc936276204c1bbd9a3f7f71cb4c79de
lawrend <douglas_lawrence@mac.com>
2017-07-21 18:33:36 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+var appendKitten = (name) => {
+  var newAr = [...kittens, name];
+  return newAr
+}
+
+var prependKitten = (name) => {
+  var newAr = [name, ...kittens];
+  return newAr
+}
+
+var removeFirstKitten = (name) => {
+  var newAr = kittens.slice(1);
+  return newAr
+}
+
+var removeLastKitten = (name) => {
+  var newAr = kittens.slice(0, kittens.length-1)
+  return newAr
+}
+
+
+var destructivelyAppendKitten = (name) => {
+  kittens.push(name)
+  return kittens
+}
+
+var destructivelyPrependKitten = (name) => {
+  kittens.unshift(name)
+  return kittens
+}
+
+var destructivelyRemoveFirstKitten = (name) => {
+  kittens.shift()
+  return kittens
+}
+
+var destructivelyRemoveLastKitten = (name) => {
+  kittens.pop()
+  return kittens
+}
