1b31dd196d8923fd919c7ea0998f83449cebefa9
haroon Azhar Khan <haroon1azhar@gmail.com>
2017-07-25 19:17:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-const app = "I don't do much."
+window.kittens =["Milo", "Otis", "Garfield"];
+function destructivelyAppendKitten(name)
+{
+  kittens.push(name);
+}
+function destructivelyPrependKitten(name)
+{
+  kittens.unshift(name);
+}
+function destructivelyRemoveLastKitten()
+{
+  kittens.pop();
+}
+function destructivelyRemoveFirstKitten()
+{
+   kittens.shift();
+}
+function appendKitten(name)
+{ var result= new Array();
+  result=[...kittens,name];
+  return result;
+}
+function prependKitten(name)
+{
+  var result= new Array();
+  result=[name,...kittens];
+  return result;
+
+}
+function removeLastKitten()
+{
+  var result= new Array();
+  result=kittens.slice(0,kittens.length-1);
+  return result;
+}
+function removeFirstKitten()
+{
+  var result= new Array();
+  result=kittens.slice(1);
+  return result;
+}
