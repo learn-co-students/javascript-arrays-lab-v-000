666f5df1a9d15273d4395730e275086a98548d87
johnsontaiwo <josseytee@gmail.com>
2017-08-01 20:31:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  var new_kittens = [...kittens, name]
+  return new_kittens
+}
+
+function prependKitten(name) {
+  var new_kittens = [name, ...kittens]
+  return new_kittens
+}
+
+function removeLastKitten() {
+  var new_kittens = kittens.slice(0, 2)
+  return new_kittens
+}
+
+function removeFirstKitten() {
+  var new_kittens = kittens.slice(1, kittens.length)
+  return new_kittens
+}
