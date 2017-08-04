ded643123b599ed5f22a2a9b30c74508a9086d38
Yevgeniy Skroznikov <ohnap@hotmail.com>
2017-08-04 00:11:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const app = "I don't do much."
+
+kittens = ["Milo","Otis","Garfield"]
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+}
+
+//function destructivelyPrependKitten(name){
+  //kitten.unshift(name)
+  //return kitten
+//}
+
+// function destructivelyRemoveLastKitten(){}
+// function destructivelyRemoveFirstKitten(){}
+// function appendKitten(name){}
+// function removeLastKitten(){}
+// function removeFirstKitten(){}
