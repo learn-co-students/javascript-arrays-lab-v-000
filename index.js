afed060ea12af58565f7e5cf167bf6cefe74e933
Pamela Bortnick <pbortnick@gmail.com>
2017-08-17 12:52:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+
+function destructivelyPrependKitten(element) {
+  return kittens.unshift(element)
+}
+
+function destructivelyAppendKitten(element) {
+  return kittens.push(element)
+}
+
+function destructivelyRemoveLastKitten(element) {
+  return kittens.pop(element)
+}
+function destructivelyRemoveFirstKitten(element) {
+  return kittens.shift(element)
+}
+
+function prependKitten(element) {
+  return [element,...kittens]
+}
+
+function appendKitten(element) {
+  return [...kittens, element]
+}
+
+function removeLastKitten(element) {
+  return kittens.slice(0, kittens.length - 1)
+}
+function removeFirstKitten(element) {
+  return kittens.slice(1)
+}
