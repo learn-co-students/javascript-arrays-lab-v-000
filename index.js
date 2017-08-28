55be3291df6d09fbea4b62d370994633f43bdbd4
github username <github email address>
2017-08-28 15:14:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens
+}
+
+
+function appendKitten(name){
+  var new_kittens = [...kittens, name]
+  return new_kittens
+}
+
+function prependKitten(name){
+
+  var new_kittens = [name, ...kittens]
+  return new_kittens
+
+}
+
+function removeLastKitten(){
+  var new_kittens = kittens.slice(0,kittens.length-1)
+  return new_kittens
+}
+
+function removeFirstKitten(){
+  var new_kittens = kittens.slice(1,kittens.length)
+  return new_kittens
+}
