485e6e43cf951d3ee64eda7ee26b8b1194339109
mirandalily <mirandaraymond@me.com>
2017-08-29 17:47:14 -0700

    2 tests passing

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ var kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendKitten(name){
   kittens.push(name)
 }
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
