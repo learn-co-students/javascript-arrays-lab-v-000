e4c43e323bc277523306b6d177fe77fd3c6a8533
mirandalily <mirandaraymond@me.com>
2017-08-29 17:48:19 -0700

    4th and 5th tests passing

--- index.js
+++ index.js
@@ -9,3 +9,11 @@ function destructivelyAppendKitten(name){
 function destructivelyPrependKitten(name){
   kittens.unshift(name)
 }
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
