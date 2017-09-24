c6e1e84e64e0c1d9afb63a794e3624ff81211be8
kromoser <kevinromoser@gmail.com>
2017-09-24 15:52:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name)
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name)
+}
+
+function appendKitten(name) {
+  var moreKittens = [...kittens, name]
+  return moreKittens
+}
+
+function prependKitten(name) {
+  var moreKittens = [name, ...kittens]
+  return moreKittens
+}
+
+function removeLastKitten(name) {
+  var moreKittens = kittens.slice(0, kittens.length - 1)
+  return moreKittens
+}
+
+function removeFirstKitten(name) {
+  var moreKittens = kittens.slice(1)
+  return moreKittens
+}
