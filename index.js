e99ae6514217bb0e92fb6d2f580f4db6ee49d19e
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2018-06-27 18:30:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much."
+
+var destructivelyAppendKitten = (name) => {
+  return window.kittens.push(name)
+}
+
+var destructivelyPrependKitten = (name) => {
+  return window.kittens.unshift(name)
+}
+
+var destructivelyRemoveLastKitten = () => {
+  return window.kittens.pop()
+}
+
+var destructivelyRemoveFirstKitten = () => {
+  return window.kittens.shift()
+}
+
+var appendKitten = (name) => {
+  return [...window.kittens, name]
+}
+
+var prependKitten = (name) => {
+  return [name, ...window.kittens]
+}
+
+var removeLastKitten = () => {
+  var newArray = window.kittens.slice(0, window.kittens.length -1)
+  return  newArray
+}
+
+var removeFirstKitten = () => {
+  return window.kittens.slice(1)
+}
